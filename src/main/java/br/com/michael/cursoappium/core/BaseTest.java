293bd9861de8a2fe780b9cf069feb37f0bdae3bc
Michael Toninger <michaeltp.2012@hotmail.com>
2021-10-01 11:22:57 -0300

    Ajustes na versao do java

--- src/main/java/br/com/michael/cursoappium/core/BaseTest.java
+++ src/main/java/br/com/michael/cursoappium/core/BaseTest.java
@@ -3,29 +3,36 @@ package br.com.michael.cursoappium.core;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
-	
+
+	@Rule
+	public TestName testName = new TestName();
+
 	@AfterClass
 	public static void finish() {
 		DriverFactory.killDriver();
 	}
-	
+
 	@After
 	public void tearDown() {
-		gerarScreenshot();
+		generateScreenshot();
 		DriverFactory.restartAPP();
 	}
-	
-	private void gerarScreenshot() {
-		final String fileName = "imagem";
+
+	private final void generateScreenshot() {
+		System.out.println("Guardando print -> " + testName.getMethodName());
+		final String fileName = testName.getMethodName();
 		final String extension = "png";
 		final String imagePath = "src/main/resources/images/" + fileName + "." + extension;
 		TakesScreenshot ts = (TakesScreenshot) DriverFactory.getDriver();
@@ -34,9 +41,11 @@ public class BaseTest {
 			BufferedImage bi = ImageIO.read(imagem);
 			File newFile = new File(imagePath);
 			ImageIO.write(bi, extension, newFile);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("PRINT SALVO!!! -> " + imagePath);
 	}
 }
